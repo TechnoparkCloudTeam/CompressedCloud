45ff587717e66f080191bc38fb0b632d610ba261
lida24 <lidalenkova204@gmail.com>
2021-04-22 18:43:59 +0300

    change all project

--- project/DataBases/Tests/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/lida/CloudProject/CompressedCloud/newproject/DataBases/Tests
-# Build directory: /home/lida/CloudProject/CompressedCloud/newproject/DataBases/Tests/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.

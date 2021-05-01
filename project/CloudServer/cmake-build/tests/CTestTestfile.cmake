347c76671e9c403a7b2ee45e5f237f0c083092b5
Rudukh <Rudukh.Eugene@yandex.ru>
2021-05-01 23:08:49 +0300

    general carcase

--- project/CloudServer/cmake-build/tests/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/lyalyashechka/TP/c_c++/cloudgitmain/CompressedCloud/CloudServer/tests
-# Build directory: /home/lyalyashechka/TP/c_c++/cloudgitmain/CompressedCloud/CloudServer/cmake-build/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
